af80f775fed3fbff9749fd1d1cbf2e5c6a48f240
Peterpaul Taekele Klein Haneveld <pp.kleinhaneveld@gmail.com>
2009-08-11 07:24:50 +0200

    Add initial generate method.

--- compiler/ClassDeclaration.c
+++ compiler/ClassDeclaration.c
@@ -8,13 +8,18 @@ O_IMPLEMENT(ClassDeclaration, void *, ctor, (void *_self, va_list *app), (_self,
 {
   struct ClassDeclaration * self = O_CAST(_self, ClassDeclaration());
   self = O_SUPER->ctor(self, app);
-  self->name = o_cast(va_arg(*app, struct Type *), Token());
   self->superclass = o_cast(va_arg(*app, struct Type *), Token());
   self->interfaces = O_BRANCH_CAST(va_arg(*app, struct RefList *), RefList());
   self->members = o_cast(va_arg(*app, struct RefList *), RefList());
   return self;
 }
 
+O_IMPLEMENT(ClassDeclaration, void, generate, (void *_self), (_self))
+{
+  struct ClassDeclaration * self = O_CAST(_self, ClassDeclaration());
+}
+
 O_OBJECT(ClassDeclaration, Declaration);
 O_OBJECT_METHOD(ClassDeclaration, ctor);
+O_OBJECT_METHOD(ClassDeclaration, generate);
 O_END_OBJECT
