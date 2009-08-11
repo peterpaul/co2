8b30746c7848076726995b0095b5b4a75c3704c9
Peterpaul Taekele Klein Haneveld <pp.kleinhaneveld@gmail.com>
2009-08-11 07:26:29 +0200

    Add generate method.

--- compiler/CompileObject.h
+++ compiler/CompileObject.h
@@ -3,8 +3,11 @@
 
 #include "RefObject.h"
 
+O_METHOD_DEF(CompileObject, void, generate, (void *_self));
+
 #define CompileObjectClass_Attr			\
-  RefObjectClass_Attr
+  RefObjectClass_Attr;				\
+  O_METHOD(CompileObject, generate)
 
 #define CompileObject_Attr			\
   RefObject_Attr
