e161d4a42f0398b9008e154754b56ed43e82d0a1
Peterpaul Taekele Klein Haneveld <pp.kleinhaneveld@gmail.com>
2009-08-11 19:41:34 +0200

    Add lookup and lookup_in_this_scope methods, and override error_already_exists.

--- compiler/Scope.c
+++ compiler/Scope.c
@@ -36,9 +36,36 @@ O_IMPLEMENT(Scope, void, leave, (void *_self), (_self))
   current_scope = self->parent;
 }
 
+O_IMPLEMENT(Scope, void, error_already_declared, (void *_self, const char * key, const void *_item), (_self, key, _item))
+{
+  struct Scope *self = O_CAST(_self, Scope());
+  struct Declaration *decl = o_cast(_item, Declaration());
+  error(decl->name, "'%s' already declared.\n", key);
+}
+
+O_IMPLEMENT(Scope, struct Declaration *, lookup_in_this_scope, (void *_self, struct Token * token), (_self, token))
+{
+  struct Scope *self = O_CAST(_self, Scope());
+  return O_CALL(self, get, token->name->data);
+}
+
+O_IMPLEMENT(Scope, struct Declaration *, lookup, (void *_self, struct Token * token), (_self, token))
+{
+  struct Scope *self = O_CAST(_self, Scope());
+  struct Declaration * result = O_CALL(self, lookup_in_this_scope, token);
+  if (result == NULL && self->parent != NULL)
+    {
+      result = O_CALL(self->parent, lookup, token);
+    }
+  return result;
+}
+
 O_OBJECT(Scope, Hash);
 O_OBJECT_METHOD(Scope, ctor);
 O_OBJECT_METHOD(Scope, dtor);
 O_OBJECT_METHOD(Scope, declare);
 O_OBJECT_METHOD(Scope, leave);
+O_OBJECT_METHOD(Scope, error_already_declared);
+O_OBJECT_METHOD(Scope, lookup_in_this_scope);
+O_OBJECT_METHOD(Scope, lookup);
 O_END_OBJECT
--- compiler/Scope.h
+++ compiler/Scope.h
@@ -3,14 +3,19 @@
 
 #include "Hash.h"
 #include "Declaration.h"
+#include "Token.h"
 
 O_METHOD_DEF(Scope, struct Declaration *, declare, (void *_self, struct Declaration * decl));
 O_METHOD_DEF(Scope, void, leave, (void *_self));
+O_METHOD_DEF(Scope, struct Declaration *, lookup_in_this_scope, (void *_self, struct Token * token));
+O_METHOD_DEF(Scope, struct Declaration *, lookup, (void *_self, struct Token * token));
 
 #define ScopeClass_Attr				\
   HashClass_Attr;				\
   O_METHOD(Scope, declare);			\
-  O_METHOD(Scope, leave)
+  O_METHOD(Scope, leave);			\
+  O_METHOD(Scope, lookup_in_this_scope);	\
+  O_METHOD(Scope, lookup)
 
 #define Scope_Attr				\
   Hash_Attr;					\
