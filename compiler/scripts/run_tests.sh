8e62d14bbe847a13ba05e57053657fcdec9876b2
Peterpaul Taekele Klein Haneveld <pp.kleinhaneveld@gmail.com>
2009-08-28 08:10:11 +0200

    Use input/output files for test verification.

--- compiler/scripts/run_tests.sh
+++ compiler/scripts/run_tests.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -u
-
 BASEDIR=`pwd`
 COMPILER=${BASEDIR}/Compiler.bin
 
-TARGET=test/target
+TESTDIR=${BASEDIR}/test
+TARGET=${TESTDIR}/target
 # Remove previous output
 rm -rf ${TARGET}
 
@@ -13,9 +13,10 @@ mkdir -p ${TARGET}/fail > /dev/null
 
 ERRORS=0
 # Testcases that should work
-TESTS=`find test/success -name "*.test"`
+TESTS=`find ${TESTDIR}/success -name "*.test"`
 for TEST in ${TESTS}
 do
+    # Compile the testcase
     BASENAME=`basename ${TEST} .test`
     TARGETNAME=${TARGET}/success/${BASENAME}
     ${COMPILER} ${TEST} ${TARGETNAME}.c > ${TARGETNAME}.err 2>&1
@@ -23,22 +24,39 @@ do
     then
 	ERRORS=$(( ERRORS + 1 ))
 	cat ${TARGETNAME}.err
-	echo "ERROR: ${TEST} failed."
+	echo "ERROR: ${TEST} failed: Compiler error"
     else
-	gcc ${TARGETNAME}.c -o ${TARGETNAME}.bin
-	if [[ "$?" != "0" ]]
+	# Compile the generated code with gcc
+	pushd `dirname ${TARGETNAME}.bin` > /dev/null 2>&1
+	gcc ${TARGETNAME}.c -o `basename ${TARGETNAME}.bin`
+	GCC_STATUS=$?
+	popd > /dev/null 2>&1
+	if [[ "${GCC_STATUS}" != "0" ]]
 	then
 	    ERRORS=$(( ERRORS + 1 ))
-	    echo "ERROR: ${TEST} failed."
+	    echo "ERROR: ${TEST} failed: GCC error"
 	else
-	    ${TARGETNAME}.bin
+	    # When no input and output exists, create empty in/output.
+	    TESTINPUT=${TESTDIR}/success/${BASENAME}.in
+	    TESTOUTPUT=${TESTDIR}/success/${BASENAME}.out
+	    if [[ ! -f ${TESTINPUT} ]]; then touch ${TESTINPUT}; fi
+	    if [[ ! -f ${TESTOUTPUT} ]]; then touch ${TESTOUTPUT}; fi
+	    # Run program with input, and compare the output with the expected output.
+	    cat  ${TESTINPUT} | ${TARGETNAME}.bin > ${TARGETNAME}.out
+	    diff ${TESTOUTPUT} ${TARGETNAME}.out
+	    if [[ "$?" != "0" ]]
+	    then
+		ERRORS=$(( ERRORS + 1 ))
+		echo "ERROR: ${TEST} failed: output error"
+	    else
+		rm ${TARGETNAME}.err
+	    fi
 	fi
-	rm ${TARGETNAME}.c ${TARGETNAME}.err
     fi
 done
 
 # Testcases that should not work
-TESTS=`find test/fail -name "*.test"`
+TESTS=`find ${TESTDIR}/fail -name "*.test"`
 for TEST in ${TESTS}
 do
     BASENAME=`basename ${TEST} .test`
@@ -50,7 +68,7 @@ do
 	cat ${TARGETNAME}.err
 	echo "ERROR: ${TEST} failed."
     else
-	rm -f ${TARGETNAME}.c ${TARGETNAME}.err
+	rm -f ${TARGETNAME}.err
     fi
 done
 
