b1f14a9a076c57d9602c2597a4368a96881122e7
Peterpaul Taekele Klein Haneveld <pp.kleinhaneveld@gmail.com>
2009-08-28 07:52:24 +0200

    Implement CompoundStatement.

--- compiler/CompoundStatement.c
+++ compiler/CompoundStatement.c
@@ -1,4 +1,5 @@
 #include "CompoundStatement.h"
+#include "io.h"
 
 #define O_SUPER Statement()
 
@@ -6,18 +7,34 @@ O_IMPLEMENT(CompoundStatement, void *, ctor, (void *_self, va_list *app), (_self
 {
   struct CompoundStatement * self = O_CAST(_self, CompoundStatement());
   self = O_SUPER->ctor(self, app);
-  /* TODO initialize */
+  self->body = O_CAST(va_arg(*app, struct RefList *), RefList());
+  O_CALL(self->body, retain);
   return self;
 }
 
 O_IMPLEMENT(CompoundStatement, void *, dtor, (void *_self), (_self))
 {
   struct CompoundStatement *self = O_CAST(_self, CompoundStatement());
-  /* TODO cleanup */
+  O_CALL(self->body, release);
   return O_SUPER->dtor(self);
+}
+
+void CompoundStatement_generate_body(void *_item)
+{
+  struct CompileObject *item = O_CAST(_item, CompileObject());
+  O_CALL(item, generate);
+}
+
+O_IMPLEMENT(CompoundStatement, void, generate, (void *_self), (_self))
+{
+  struct CompoundStatement *self = O_CAST(_self, CompoundStatement());
+  fprintf(out, "{\n");
+  O_CALL(self->body, map, CompoundStatement_generate_body);
+  fprintf(out, "}\n");
 }
 
 O_OBJECT(CompoundStatement, Statement);
 O_OBJECT_METHOD(CompoundStatement, ctor);
 O_OBJECT_METHOD(CompoundStatement, dtor);
+O_OBJECT_METHOD(CompoundStatement, generate);
 O_END_OBJECT
--- compiler/CompoundStatement.h
+++ compiler/CompoundStatement.h
@@ -2,12 +2,14 @@
 #define CompoundStatement_H
 
 #include "Statement.h"
+#include "RefList.h"
 
 #define CompoundStatementClass_Attr			\
   StatementClass_Attr
 
 #define CompoundStatement_Attr				\
-  Statement_Attr
+  Statement_Attr;					\
+  struct RefList * body
 
 O_CLASS(CompoundStatement, Statement);
 
