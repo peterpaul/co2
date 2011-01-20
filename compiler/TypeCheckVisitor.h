c698e9a6e3a5fac9605195a418d819e598c6acf0
Peterpaul Taekele Klein Haneveld <pp.kleinhaneveld@gmail.com>
2011-01-20 09:38:38 +0100

    Initial version of TypeCheckVisitor.

---
+++ compiler/TypeCheckVisitor.h
@@ -0,0 +1,14 @@
+#ifndef TypeCheckVisitor_H
+#define TypeCheckVisitor_H
+
+#include "BaseCompileObjectVisitor.h"
+
+#define TypeCheckVisitorClass_Attr		\
+  BaseCompileObjectVisitorClass_Attr
+
+#define TypeCheckVisitor_Attr		\
+  BaseCompileObjectVisitor_Attr
+
+O_CLASS(TypeCheckVisitor, BaseCompileObjectVisitor);
+
+#endif /* TypeCheckVisitor_H */
