6db58371c059a19073df03c1f1f701d01c7d4be4
Peterpaul Taekele Klein Haneveld <pp.kleinhaneveld@gmail.com>
2011-02-02 22:39:14 +0100

    Align logfile with run_succes_test.sh.

--- carbon/test/run_fail_test.sh
+++ carbon/test/run_fail_test.sh
@@ -6,20 +6,27 @@ COMPILER=${BASEDIR}/../src/carbon
 TESTDIR=${BASEDIR}
 TARGET=${TESTDIR}/target
 
+LOGFILE=${TARGET}/${!#}.log
+if [[ -e ${LOGFILE} ]]
+then
+    rm -f ${LOGFILE}
+fi
+
 mkdir -p ${TARGET}/fail
 find fail -name "*.h" -exec cp -t ${TARGET}/fail {} \;
 
 function run_fail_test {
+    echo "--- ${LOGFILE} ---"
     local TEST=$1
     local BASENAME=`basename ${TEST} .test`
     local TARGETNAME=${TARGET}/fail/${BASENAME}
-    ${COMPILER} ${TEST} ${TARGETNAME}.c > ${TARGETNAME}.err 2>&1
+    echo "Command: ${COMPILER} ${TEST} ${TARGETNAME}.c" >> ${LOGFILE}
+    ${COMPILER} ${TEST} ${TARGETNAME}.c >> ${LOGFILE} 2>&1
     if [[ "$?" == "0" ]]
     then
-	echo "Command: ${COMPILER} ${TEST} ${TARGETNAME}.c" >> ${TARGETNAME}.err
 	return 1
     fi
     return 0
 }
 
-run_fail_test $1
+run_fail_test ${!#}
