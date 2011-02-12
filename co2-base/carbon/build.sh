0f729805b73ab04a131e9be91207f2eecaa337da
Peterpaul Taekele Klein Haneveld <pp.kleinhaneveld@gmail.com>
2011-02-12 09:29:19 +0100

    First simple implementation of logging framework.

--- co2-base/carbon/build.sh
+++ co2-base/carbon/build.sh
@@ -1,6 +1,16 @@
+#!/bin/bash
+set -x
+
+LIBSOURCES="Array.c ArrayList.c Exception.c IndexOutOfBoundsException.c\
+	Iterator.c List.c ListIterator.c Object.c OutOfMemoryException.c\
+	RefObject.c ReleasePool.c ReleasePoolItem.c Singleton.c String.c\
+	Level.c Logger.c LogFormatter.c LogHandler.c LogRecord.c\
+	Comparable.c ConsoleHandler.c SimpleFormatter.c"
+
 rm *.[ch]
 rm a.out
 make clean
-make -k -j3
-make -k -j3
-gcc -O0 -g3 -I/home/peterpaul/local/co2-0.0.1/include/co2-1.0 -L/home/peterpaul/local/co2-0.0.1/lib -lco2-1.0 *.c
+make -k
+make -k
+gcc -O0 -g3 -I/home/peterpaul/local/co2-0.0.1/include/co2-1.0 -L/home/peterpaul/local/co2-0.0.1/lib -lco2-1.0 $LIBSOURCES TestLogger.c -o TestLogger.bin
+gcc -O0 -g3 -I/home/peterpaul/local/co2-0.0.1/include/co2-1.0 -L/home/peterpaul/local/co2-0.0.1/lib -lco2-1.0 $LIBSOURCES TestRefObject.c -o TestRefObject.bin
