8a25929d6e7e1e522fd9a01b9c8a37444b51626e
Peterpaul Taekele Klein Haneveld <pp.kleinhaneveld@gmail.com>
2011-03-02 07:38:41 +0100

    Move source files relative to co2.

--- co2-base/carbon/build.sh
+++ co2-base/carbon/build.sh
@@ -1,51 +1,51 @@
 #!/bin/bash
 set -x
 
-LIBSOURCES="Array.c
-ArrayIterator.c
-ArrayList.c
-Collection.c
-Comparable.c
-ConsoleHandler.c
-Enum.c
-Exception.c
-HashMap.c
-HashMapIterator.c
-HashSet.c
-HashSetIterator.c
-IndexOutOfBoundsException.c
-Iterable.c
-Iterator.c
-KeyValuePair.c
-Level.c
-List.c
-ListIterator.c
-LogFormatter.c
-Logger.c
-LogHandler.c
-LogRecord.c
-Map.c
-Object.c
-OutOfMemoryException.c
-RefObject.c
-ReleasePool.c
-ReleasePoolItem.c
-Set.c
-SimpleFormatter.c
-String.c
-Throwable.c"
+LIBSOURCES="co2/Array.c
+co2/ArrayIterator.c
+co2/ArrayList.c
+co2/Collection.c
+co2/Comparable.c
+co2/ConsoleHandler.c
+co2/Enum.c
+co2/Exception.c
+co2/HashMap.c
+co2/HashMapIterator.c
+co2/HashSet.c
+co2/HashSetIterator.c
+co2/IndexOutOfBoundsException.c
+co2/Iterable.c
+co2/Iterator.c
+co2/KeyValuePair.c
+co2/Level.c
+co2/List.c
+co2/ListIterator.c
+co2/LogFormatter.c
+co2/Logger.c
+co2/LogHandler.c
+co2/LogRecord.c
+co2/Map.c
+co2/Object.c
+co2/OutOfMemoryException.c
+co2/RefObject.c
+co2/ReleasePool.c
+co2/ReleasePoolItem.c
+co2/Set.c
+co2/SimpleFormatter.c
+co2/String.c
+co2/Throwable.c"
 
 if [[ ($# > 0) && ("$1" == "clean") ]]
 then
-    rm *.[ch]
-    rm a.out
     make clean
+    rm co2/*.[ch]
+    rm TestLogger.bin TestRefObject.bin TestMap.bin TestSet.bin
 else
     make -k
     make -k
     rm TestLogger.bin TestRefObject.bin TestMap.bin TestSet.bin
-    gcc -O0 -g3 -I/home/peterpaul/local/co2-0.0.2/include/co2-1.0 -L/home/peterpaul/local/co2-0.0.2/lib -lco2-1.0 $LIBSOURCES TestLogger.c -o TestLogger.bin
-    gcc -O0 -g3 -I/home/peterpaul/local/co2-0.0.2/include/co2-1.0 -L/home/peterpaul/local/co2-0.0.2/lib -lco2-1.0 $LIBSOURCES TestRefObject.c -o TestRefObject.bin
-    gcc -O0 -g3 -I/home/peterpaul/local/co2-0.0.2/include/co2-1.0 -L/home/peterpaul/local/co2-0.0.2/lib -lco2-1.0 $LIBSOURCES TestMap.c -o TestMap.bin
-    gcc -O0 -g3 -I/home/peterpaul/local/co2-0.0.2/include/co2-1.0 -L/home/peterpaul/local/co2-0.0.2/lib -lco2-1.0 $LIBSOURCES TestSet.c -o TestSet.bin
+    gcc -O0 -g3 -I/home/peterpaul/local/co2-0.0.2/include/co2-1.0 -L/home/peterpaul/local/co2-0.0.2/lib -lco2-1.0 $LIBSOURCES co2/TestLogger.c -o TestLogger.bin
+    gcc -O0 -g3 -I/home/peterpaul/local/co2-0.0.2/include/co2-1.0 -L/home/peterpaul/local/co2-0.0.2/lib -lco2-1.0 $LIBSOURCES co2/TestRefObject.c -o TestRefObject.bin
+    gcc -O0 -g3 -I/home/peterpaul/local/co2-0.0.2/include/co2-1.0 -L/home/peterpaul/local/co2-0.0.2/lib -lco2-1.0 $LIBSOURCES co2/TestMap.c -o TestMap.bin
+    gcc -O0 -g3 -I/home/peterpaul/local/co2-0.0.2/include/co2-1.0 -L/home/peterpaul/local/co2-0.0.2/lib -lco2-1.0 $LIBSOURCES co2/TestSet.c -o TestSet.bin
 fi
