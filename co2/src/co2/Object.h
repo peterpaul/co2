95541d227d7ca18e0ce2978d30dba8a7cb4692ac
Peterpaul Taekele Klein Haneveld <pp.kleinhaneveld@gmail.com>
2011-03-09 18:32:39 +0100

    Add copyright and license information and comment cleanup.

--- co2/src/co2/Object.h
+++ co2/src/co2/Object.h
@@ -16,13 +16,6 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-/*
- * Object.h
- *
- *  Created on: Oct 22, 2008
- *      Author: epetkle
- */
-
 #ifndef OBJECT_H_
 #define OBJECT_H_
 
