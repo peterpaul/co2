c4eae785468531aa32b0bbf4232f3e486f4f5469
Peterpaul Taekele Klein Haneveld <pp.kleinhaneveld@gmail.com>
2011-03-16 18:28:35 +0100

    Add example autotools project.

---
+++ examples/my-object/src/MyObject.c
@@ -0,0 +1,48 @@
+/*
+ * libco2 Library for object oriented programming in C
+ *
+ * Copyright (C) 2011 Peterpaul Taekele Klein Haneveld
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "MyObject.h"
+
+#define O_SUPER Object()
+
+void *MyObject_ctor(void *_self, va_list * argp)
+{
+	struct MyObject *self = o_cast(_self, MyObject());
+	self = O_SUPER->ctor(self, argp);
+	self->value = va_arg(*argp, int);
+	return self;
+}
+
+int MyObject_getValue(void *_self)
+{
+	struct MyObject *self = o_cast(_self, MyObject());
+	return self->value;
+}
+
+int MyObject_setValue(void *_self, int value)
+{
+	struct MyObject *self = o_cast(_self, MyObject());
+	self->value = value;
+	return value;
+}
+
+O_OBJECT(MyObject, Object);
+self->ctor = MyObject_ctor;
+self->getValue = MyObject_getValue;
+self->setValue = MyObject_setValue;
+O_OBJECT_END
---
+++ examples/my-object/src/MyObject.h
@@ -0,0 +1,38 @@
+/*
+ * libco2 Library for object oriented programming in C
+ *
+ * Copyright (C) 2011 Peterpaul Taekele Klein Haneveld
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef _MYOBJECT_H
+#define _MYOBJECT_H
+
+#include "co2/Object.h"
+
+O_METHOD_DEF(MyObject, int, getValue, (void *_self));
+O_METHOD_DEF(MyObject, int, setValue, (void *_self, int value));
+
+#define MyObjectClass_Attr					\
+	ObjectClass_Attr;						\
+	O_METHOD(MyObject, getValue);			\
+	O_METHOD(MyObject, setValue)
+
+#define MyObject_Attr						\
+	Object_Attr;							\
+	int value
+
+O_CLASS(MyObject, Object);
+
+#endif				/* _MYOBJECT_H */
